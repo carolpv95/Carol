3357b080f27e9d07818216d826973360d4a9e456
Carol <19carolpv@gmail.com>
2024-02-07 13:19:58 -0300

    aula 1 html

--- carol.js
+++ carol.js
@@ -1,6 +1,12 @@
-if(i) {
-    
-}
-else(a) {
+const a = 10
+const b = 5
 
-}
+function 
+    if (a == b) {
+        console.log('ja pode votar')
+    } else {
+        console.log('Ainda é muito cedo para votar')
+    }
+
+
+    
